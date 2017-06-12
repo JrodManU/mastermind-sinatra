c316f733a34b8bfa568e7b13a020e0b2fca14eee
JrodManU <jrodmanu@yahoo.com>
2017-06-12 11:43:51 -0400

    game functions

--- code_handler.rb
+++ code_handler.rb
@@ -1,43 +1,38 @@
 class CodeHandler
-	class Code
-		attr_reader :value
-		def initialize(value)
-			@value = value
-		end
-	end
+  attr_accessor :secret_code
+	@@allowed_chars = ['a','b','c','d','e','f']
 
-	@@allowed_chars = ["a","b","c","d","e","f"]
+  def initialize
+    new_code
+  end
 
-	def initialize
+	def new_code
 		@secret_code = random_code
 	end
 
-	def create_code(code)
-		Code.new(code) if code.length == 4 && good_code(code)
-	end
-
-	def good_code(code)
-		code.downcase!
-		return true if code.split("").all? do |char|
+	def valid_code(code)
+		code.length == 4 && code.downcase.split('').all? do |char|
 			@@allowed_chars.include?(char)
 		end
-		false
 	end
 
+  def correct_code(code)
+    code.downcase.eql?(@secret_code)
+  end
+
 	def random_code
-		code = @@allowed_chars.sample(4).join("")
+		code = @@allowed_chars.sample(4).join('')
 	end
 
 	def rate(guess)
-		guess.downcase!
 		rating = []
-		guess.split("").each_with_index do |letter, index|
+		guess.downcase.split('').each_with_index do |letter, index|
 			if letter == @secret_code[index]
-				rating << "R"
+				rating << 'R'
 			elsif @secret_code.include?(letter)
-				rating << "W"
+				rating << 'W'
 			end
 		end
-		rating.sort.join("")
+		rating.sort.join('')
 	end
 end
--- mastermind.rb
+++ mastermind.rb
@@ -2,8 +2,29 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-require './code_handler.rb'
+require_relative 'code_handler'
+
+enable :sessions
+set :session_secret, 'tjwa9*&%Y28woHTGN@Y#8tysejzhg#O*53lWy'
 
 get '/' do
-  erb :game
+  if !session['code_handler']
+    session['code_handler'] = CodeHandler.new
+  end
+
+  message = ''
+  correct = false
+  if params['restart']
+     session['code_handler'].new_code
+  end
+  if params['guess']
+    guess = params['guess']
+    if  session['code_handler'].valid_code(guess)
+      message =  session['code_handler'].rate(guess)
+      correct =  session['code_handler'].correct_code(guess)
+    else
+      message = 'Please make sure your guess is 4 letters and a-f only.'
+    end
+  end
+  erb :game, :locals => { :message => message, :correct => correct }
 end
