d8b7c18b651c88cd2232adde9ba205ca9592f024
JrodManU <jrodmanu@yahoo.com>
2017-06-12 11:54:46 -0400

    added guess history

--- mastermind.rb
+++ mastermind.rb
@@ -10,6 +10,7 @@ set :session_secret, 'tjwa9*&%Y28woHTGN@Y#8tysejzhg#O*53lWy'
 get '/' do
   if !session['code_handler']
     session['code_handler'] = CodeHandler.new
+    session['past_guesses'] = []
   end
 
   message = ''
@@ -22,9 +23,10 @@ get '/' do
     if  session['code_handler'].valid_code(guess)
       message =  session['code_handler'].rate(guess)
       correct =  session['code_handler'].correct_code(guess)
+      session['past_guesses'].unshift(guess + ' - ' + message)
     else
       message = 'Please make sure your guess is 4 letters and a-f only.'
     end
   end
-  erb :game, :locals => { :message => message, :correct => correct }
+  erb :game, :locals => { :message => message, :correct => correct, :past_guesses => session['past_guesses'] }
 end
